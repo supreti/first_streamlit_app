2e86de9fa7e09e4d382882f02d593da64ef97f06
Subodh Upreti <50602433+supreti@users.noreply.github.com>
2023-06-17 18:19:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
-streamlit.title('My first exposure to streamlit')
-streamlit.title('This is quite exciting')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
