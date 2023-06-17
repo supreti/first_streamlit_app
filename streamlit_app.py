f885c8cabd1f7d551aa0915a51d1cb8595988883
Subodh Upreti <50602433+supreti@users.noreply.github.com>
2023-06-17 18:26:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ streamlit.text(' 🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+treamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
