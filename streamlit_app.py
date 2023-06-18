082ff52d67afcc521289f31b55fb6bd6863feca5
Subodh Upreti <50602433+supreti@users.noreply.github.com>
2023-06-18 13:34:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.dataframe(my_fruit_list)
 
 stremlit.header('Fruityvice fruit advice')
 
-import request
+import requests
 fruityvice_response = request.get('https://fruityvice.com/api/fruit/watermelon')
 stremlit.text(fruityvice_response.json())
 
