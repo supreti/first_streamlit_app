bce98080debb23164acbaf9bbb45c39cbf6fc2cb
Subodh Upreti <50602433+supreti@users.noreply.github.com>
2023-06-18 13:38:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
-stremlit.header('Fruityvice fruit advice')
+streamlit.header('Fruityvice fruit advice')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-stremlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())
 
 
 
