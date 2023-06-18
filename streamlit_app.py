43dc4172181e667be3fd159768fbbaca0e0866d7
Subodh Upreti <50602433+supreti@users.noreply.github.com>
2023-06-18 16:53:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import snowflake-connector
 streamlit.header('reakfast Menu')
 streamlit.text(' 🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
