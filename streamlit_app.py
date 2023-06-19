be95c5d816b2c01af85baf8e6950cc2346a91e9f
Subodh Upreti <50602433+supreti@users.noreply.github.com>
2023-06-19 15:08:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ except URLError as e:
    streamlit.error()
 
 
-streamlit.header('The Fruit load list contains")
+streamlit.header('The Fruit load list contains')
 if streamlit.button("Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
