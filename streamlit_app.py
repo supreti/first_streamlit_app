d56fc99e556d63c7e2533bb09c472bca9b15b80c
Subodh Upreti <50602433+supreti@users.noreply.github.com>
2023-06-19 14:20:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,22 +25,20 @@ fruits_to_show =my_fruit_list.loc[fruits_selected]
 # Now display  the table on the page
 streamlit.dataframe(fruits_to_show)
 
-# Time to get detail on fruits
-
-
+def  get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+  
+#New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit advice!')
 try:
   fruit_choice = streamlit.text_input('what would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    ####streamlit.text(fruityvice_response.json())
-    # Make it look prettier
-    #following line take the semi structurd json file and convert into flat table.
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    #dislpay data in tabluar format
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function =get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(fback_from_function)
 except URLError as e:
    streamlit.error()
 
