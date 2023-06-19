159fe070b91b1622d51068bf1940618d2d446482
Subodh Upreti <50602433+supreti@users.noreply.github.com>
2023-06-19 10:50:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,8 @@ streamlit.dataframe(my_data_row)
 
 # prompt user to add new food
 fruit_to_add = streamlit.text_input('what fruit would you like to add?')
-mysql = ("INSERT INTO fruit_load_list (fruit_name)  VALUES (%s)", (fruit_to_add))
+print(f"Hello, My name is {name} and I'm {age} years old.")
+mysql = f"INSERT INTO fruit_load_list (fruit_name)  VALUES {fruit_to_add}"
 my_cur.execute (mysql)
 streamlit.text("Thank for adding" + fruit_to_add)
 
