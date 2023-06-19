7465023c415df3c72c4b80832d8e7257b15c9df9
Subodh Upreti <50602433+supreti@users.noreply.github.com>
2023-06-19 12:19:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit
 import pandas
 import snowflake.connector
+import requests
+
+###############################################
+# Build Menu
+###############################################
+
 streamlit.header('Menu from our Healty Kitchen')
 streamlit.text(' 🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
@@ -8,11 +14,16 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# let's put a pick list here so visitor can pick pick fruit they want
+streamlit.multiselect("Pick some fruit:",list(my_fruit_list.index))
+
+# Now display  the table on the page
 streamlit.dataframe(my_fruit_list)
+
 
 streamlit.header('Fruityvice fruit advice')
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 # Make it look prettier
